1ed8990fe881cbc15c144b4be68b3fd790ccab30
Cuda-Chen <clh960524@gmail.com>
2019-02-14 15:58:39 +0800

    add stuffs

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # barrel-distortion-cpp
 A barrel distortion example written in C++
+
+# Dependencies
+* CMake
+* OpenCV
+
+# How to Compile and Run
+If you are using UNIX-like system type these commands:
+```
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+$ cd ..
+$ ./barrel_distort_cpp
+```
+
+# Reference
+The work of barrel distortion is originated from 逍遙文工作室. <br>
+You can find his work in the following link (Chinese website):
+* https://cg2010studio.com/2012/01/03/opencv-%E6%A8%A1%E6%93%AC%E9%AD%9A%E7%9C%BC%E9%8F%A1%E9%A0%AD-simulate-fisheye-lens/
---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+#include "opencv2/opencv.hpp"
+#include "barrel_distortion.hpp"
+
+int main(int argc, char **argv)
+{
+	using namespace std;
+	using namespace cv;
+
+	Mat input, output;
+	float K;
+	float centerX, centerY;
+	int width, height;
+
+	if(argc == 3)
+	{
+		input = imread(argv[1]);
+		output = Mat(input.rows, input.cols, input.type);
+		K = atof(argv[2]);
+		width = input.cols;
+		height = input.rows;
+		centerX = 
+	}
+	else if(argc == 5)
+	{
+	}
+	else
+	{
+		cout << "usage: ./barrel_distort_cpp \
+			<input image> \
+			<K: coefficient of distortion> \
+			[x corrdinate of center of distortion (in pixel)] \
+			[y corrdinate of center of distortion (in pixel]"
+			<< endl;
+		return 1;
+	}
+
+	// barrel distort here
+
+	return 0;
+}
